6c2ed000bd78ed2546930c718c9debd5faba6b1d
acwfls <wyffels.ac@icloud.com>
2023-10-17 14:50:06 +0200

    5-print

---
+++ variables_if_else_while/5-print_number.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
