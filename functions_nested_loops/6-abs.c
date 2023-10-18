3e161fb8ca39f7c0251d6541c2a5ff0c65be43f8
acwfls <wyffels.ac@icloud.com>
2023-10-18 20:59:55 +0200

    6-abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _abs(int) - Enter point
+*
+* Description: computes the absolute value of an integer.
+* @i: absolute value being computed
+*
+* Return: 0 or 1 or -1
+*/
+
+int _abs(int i)
+
+{
+	if (i > 0)
+	{
+	return (i);
+	}
+	else if (i < 0)
+	{
+	return (-i);
+	}
+	else
+	{
+	return (0);
+	}
+}
