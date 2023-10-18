2e8ee266b78685a776dcdea4f14c0439a918d425
acwfls <wyffels.ac@icloud.com>
2023-10-18 22:14:18 +0200

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - Entry point
+*
+* Description: adds two integers and returns the result.
+* @n: first integer
+* @m: second integer
+*
+* Return: 0
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
