3ce073dcc2f0beea29b6a3948a53095ca48c7a35
acwfls <wyffels.ac@icloud.com>
2023-10-26 23:05:42 +0200

    1-strncat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
