1d8f9b99a6ed9d35635ef96be711b7a83ab3c0fc
acwfls <wyffels.ac@icloud.com>
2023-10-29 08:49:06 +0100

    5-string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
